75b57b793e059f2177e3935f63345dfcca98c397
botong719 <botong719@gmail.com>
2019-06-01 18:46:30 -0700

    adding files to repository

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Balboa Burger", false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("California Burgito", false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Pastrami Burger", false);
